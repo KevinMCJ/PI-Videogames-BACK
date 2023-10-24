9720a0881aaa2e0ea500386bf1265d712943fcfc
KevinMCJ <kevin72309@gmail.com>
2023-10-24 16:48:03 -0300

    update front-end deploy

--- src/app.js
+++ src/app.js
@@ -15,14 +15,20 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  const allowedOrigins = ['https://pi-videogames-front-production-b21d.up.railway.app', 'http://localhost:5173'];
+  const allowedOrigins = [
+    'https://gamedex-pi.netlify.app/',
+    'http://localhost:5173',
+  ];
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
@@ -30,7 +36,8 @@ server.use((req, res, next) => {
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
